ea53db3902ac359efd23e321d303cf6333cdfbce
Luiz Ricardo <luizricardo41@gmail.com>
2021-10-25 12:51:02 -0300

    Implementaçao da funçao para cumprir o requisito 3

--- script.js
+++ script.js
@@ -28,15 +28,16 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  const removeItem = event.target;
+  removeItem.parentNode.removeChild(removeItem);
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
