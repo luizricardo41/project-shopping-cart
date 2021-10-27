3ac5a8db3a9fc22ab20b14367243391597c65672
Luiz Ricardo <luizricardo41@gmail.com>
2021-10-27 20:51:41 -0300

    Implementaçao de funçao e testes para cumprir requisito 6

--- script.js
+++ script.js
@@ -28,6 +28,36 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// const createElementPriceTotal = () => {
+//   const createSpan = document.createElement('span');
+//   createSpan.className = 'total-price';
+//   return createSpan;
+// };
+
+// function updateValues(totalPrice) {
+//   const verifyItem = document.querySelector('.total-price');
+//   if (!verifyItem) {
+//     const selectLocal = document.querySelector('.cart');
+//     const spanElement = createElementPriceTotal();
+//     selectLocal.appendChild(spanElement);
+//     document.getElementsByClassName('total-price').innerText = `${totalPrice}`;
+//   } else {
+//     verifyItem.innerText = `${totalPrice}`;
+//   }
+// }
+
+// const classDivPrices = '.total-price';
+
+// const subPriceTotal = async (id) => {
+//   const verifyItem = document.querySelector(classDivPrices);
+//   const itemSelected = await fetchItem(id);
+//   const priceItem = itemSelected.price;
+//   const priceTotal = verifyItem.innerText;
+//   const convertString = parseFloat(priceTotal);
+//   const subPrices = (convertString - priceItem).toFixed(2);
+//   verifyItem.innerText = `${subPrices}`;
+// };
+
 const removeItemsStorage = (textItem) => {
   const arraySaved = localStorage.getItem('cartItems');
   const removeId = `${textItem.slice(5, 19)}`;
@@ -35,6 +65,7 @@ const removeItemsStorage = (textItem) => {
   const findItemSaved = arrayStorage.find((value) => removeId.includes(value));
   const indexOfId = arrayStorage.indexOf(findItemSaved);
   arrayStorage.splice(indexOfId, 1);
+  // subPriceTotal(removeId);
   saveCartItems(arrayStorage);
 };
 
@@ -55,6 +86,7 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 const searchSku = async (skuItem) => {
   const itemSku = await fetchItem(skuItem);
+  console.log(skuItem);
   const objectItem = {
     sku: itemSku.id,
     name: itemSku.title,
@@ -64,6 +96,24 @@ const searchSku = async (skuItem) => {
   const liElements = createCartItemElement(objectItem);
   createOlElements.appendChild(liElements);
 };
+
+// const sumPriceTotal = async (id) => {
+//   const itemSelected = await fetchItem(id);
+//   const priceItem = itemSelected.price;
+//   const verifyItem = document.querySelector(classDivPrices);
+//   if (!verifyItem) {
+//     const selectLocal = document.querySelector('.cart');
+//     const spanElement = createElementPriceTotal();
+//     selectLocal.appendChild(spanElement);
+//     const sumElement = document.querySelector(classDivPrices);
+//     sumElement.innerText = `${priceItem}`;
+//   } else {
+//     const priceTotal = verifyItem.innerText;
+//     const convertString = parseFloat(priceTotal);
+//     const sumPrices = (convertString + priceItem).toFixed(2);
+//     verifyItem.innerText = `${sumPrices}`;
+//   }
+// };
 
 const saveCartInStorage = (id) => {
   let arrayItems = [];
@@ -81,6 +131,7 @@ const saveCartInStorage = (id) => {
 const findSKU = () => {
   const skuItem = document.querySelector('#selected');
   const idItem = getSkuFromProductItem(skuItem);
+  // sumPriceTotal(idItem);
   saveCartInStorage(idItem);
   searchSku(idItem);
 };
@@ -100,13 +151,33 @@ function initializeButtons() {
 }
 
 const receiveDataSaved = () => {
+  // const priceSaved = localStorage.getItem('prices');
   const arraySaved = getSavedCartItems();
   if (arraySaved !== undefined) {
     const arrayStorage = arraySaved.split(',');
     arrayStorage.forEach((element) => {
       searchSku(element);
     });
+    // const selectLocal = document.querySelector('.cart');
+    // const spanElement = createElementPriceTotal();
+    // selectLocal.appendChild(spanElement);
+    // const sumPrice = document.querySelector('.total-price');
+    // sumPrice.innerText = `${priceSaved}`;
   }
+};
+
+const clearCartItems = () => {
+  const cartItems = document.querySelectorAll('.cart__item');
+  const parentCartItems = document.querySelector('.cart__items');
+  cartItems.forEach((element) => {
+    parentCartItems.removeChild(element);
+  });
+  localStorage.clear();
+};
+
+const clearButton = () => {
+  const pushButton = document.querySelector('.empty-cart');
+  pushButton.addEventListener('click', clearCartItems);
 };
 
 const implementData = async (product) => {
@@ -121,6 +192,7 @@ const implementData = async (product) => {
     const sectionElements = createProductItemElement(objectElement);
     createSectionElements.appendChild(sectionElements);
   });
+  clearButton();
   initializeButtons();
   receiveDataSaved();
 };
